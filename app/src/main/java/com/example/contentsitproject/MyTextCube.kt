ba23c729f8890133af8f3ec2ef1c47c5769cb270
MyungJewon <jj4234@naver.com>
2021-12-08 16:08:33 +0900

    1208

--- app/src/main/java/com/example/contentsitproject/MyTextCube.kt
+++ app/src/main/java/com/example/contentsitproject/MyTextCube.kt
@@ -22,10 +22,10 @@ class MyTextCube(private val myContext: Context){
     private val projectionMatrix=FloatArray(16)
     private val viewMatrix=FloatArray(16)
     private val modelMatrix=FloatArray(16)
-    private var vertexCoords=
+    private var vertexCoords2=
         floatArrayOf(
             44.0f, -45.0f, 20.0f, 44.0f, -45.0f, 0.0f, 44.0f, -25.0f, 0.0f, 44.0f, -25.0f, 20.0f, 24.0f, -25.0f, 20.0f, 24.0f, -45.0f, 20.0f, 24.0f, -25.0f, 0.0f, 24.0f, -45.0f, 0.0f)
-    private var vertexCoords1=
+    private var vertexCoords=
         floatArrayOf(
         -0.5f, 0.5f,-0.5f,
          0.5f, 0.5f,-0.5f,
