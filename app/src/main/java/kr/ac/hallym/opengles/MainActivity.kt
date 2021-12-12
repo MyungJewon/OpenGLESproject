10d6f7ba7f73b705b69cbda22373fc3d219a9684
MYUNG\jj423 <jj4234@naver.com>
2021-12-12 18:26:57 +0900

    내부 저장소 리스트 불러오기

--- app/src/main/java/kr/ac/hallym/opengles/MainActivity.kt
+++ app/src/main/java/kr/ac/hallym/opengles/MainActivity.kt
@@ -1,13 +1,18 @@
 package kr.ac.hallym.opengles
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
-import java.io.BufferedReader
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.BaseAdapter
+import android.widget.ListView
+import android.widget.TextView
 import java.io.File
-import java.io.FileReader
 
 var drawMode: Int = -1
 val objvertex= ArrayList<Float>()
@@ -17,27 +22,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val listView = findViewById<ListView>(R.id.listView)
+        val path = File(filesDir.toString())
+        val files = path.listFiles()
+        val strFileList = ArrayList<String>()
+        val fileSize = ArrayList<String>()
+        for(file in files){
+            strFileList +=file.name
+            fileSize +=file.length().toString()
+        }
+//        어답터 설정
+        listView.adapter = MyCustomAdapter(this,strFileList,fileSize)
+
+        listView.onItemClickListener=AdapterView.OnItemClickListener{parent, view, position, id ->
+        val selectItem = parent.getItemAtPosition(position) as String
+        drawCube(selectItem)
+        }
     }
 
-    fun drawTriangle(view: View){
-        drawMode = 0;
-        val intent = Intent(this,MainGLActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun drawSquare(view: View){
-        drawMode = 1;
-        val intent = Intent(this,MainGLActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun drawCube(view: View){
+    fun drawCube(view: String){
         drawMode = 2;
 
-        var objscr:String=filesDir.toString()+"/xXing_non.obj"
+        objvertex.clear()
+        objface.clear()
+        objcolor.clear()
+        var objscr:String=filesDir.toString()+"/"+view
         var objread:String=File(objscr).bufferedReader().use { it.readText()}
         val objlist=objread.split("\n")
-
+        Log.d("Log1",objscr)
         for(i in objlist){
             if(i.indexOf("v")==0){
                 val j = i.replace("v ", "")
@@ -52,29 +64,43 @@ class MainActivity : AppCompatActivity() {
                     objface.add((q.toFloat().toInt()-1).toShort())
             }
         }
-//        Log.d("Log1", objvertex.toString()+"f")
-//        Log.d("Log2", objface.toString())
+        Log.d("Log2", objvertex.size.toString())
+        Log.d("Log3", objface.size.toString())
         for(i in 0 until objvertex.size)
             objcolor.add(Math.random().toFloat())
         val intent = Intent(this,MainGLActivity::class.java)
         startActivity(intent)
 
     }
+}
+    private class MyCustomAdapter(context: Context, strFileList: ArrayList<String>,fileSize: ArrayList<String>) : BaseAdapter() {
+        private val mContext: Context
+        val list=strFileList
+        val size=fileSize
+    init {
+        mContext = context
+    }
+    override fun getCount(): Int {
+        return list.size
+    }
 
-    fun drawHexagonalPyramid(view: View){
-        drawMode = 3;
-        val intent = Intent(this,MainGLActivity::class.java)
-        startActivity(intent)
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
     }
-    fun drawTextCube(view: View){
-        drawMode = 4;
-        val intent = Intent(this,MainGLActivity::class.java)
-        startActivity(intent)
+    override fun getItem(position: Int): Any {
+        val selectItem = list.get(position)
+        return selectItem
     }
-    fun drawLitCube(view: View){
-        drawMode = 5;
-        val intent = Intent(this,MainGLActivity::class.java)
-        startActivity(intent)
+    override fun getView(position: Int, view: View?, viewGroup: ViewGroup?): View {
+        val layoutInflater = LayoutInflater.from(mContext)
+        val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
+
+        val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
+        nameTextView.text = list.get(position)
+        val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+        positionTextView.text = "용량 : "+size.get(position)+"Byte"
+
+        return rowMain
     }
 
 }
--- app/src/main/java/kr/ac/hallym/opengles/MainGLRenderer.kt
+++ app/src/main/java/kr/ac/hallym/opengles/MainGLRenderer.kt
@@ -17,99 +17,30 @@ val objvertex1 = objvertex.toFloatArray()
 val objface1 = objface.toShortArray()
 val objcolor1= objcolor.toFloatArray()
 class MainGLRenderer(private val myContext: Context): GLSurfaceView.Renderer{
-
-    private lateinit var mTriangle: MyTriangle
-    private lateinit var mSquare: MySquare
     private lateinit var mCube: MyCube
-    private lateinit var mHexagonalPyramid: MyHexagonalPyramid
     private lateinit var mTrackball: MyTrackball
-    private lateinit var mTexCube: MyTextCube
-    private lateinit var mTexGround: MyTexGround
-    private lateinit var mTexPillar: MyTexPillar
-    private lateinit var mLitCube:MyLitCube
-    
- //   private val mvpMatrix = FloatArray(16)
-   // private val projectionMatrix = FloatArray(16)
-   // private val viewMatrix = FloatArray(16)
-
-
     override fun onSurfaceCreated(unused: GL10, config: EGLConfig) {
-        // Set the background frame color
         GLES30.glClearColor(0.2f, 0.2f, 0.2f, 1.0f)
 
         GLES30.glEnable(GLES30.GL_DEPTH_TEST)
 
         when(drawMode){
-            0 -> mTriangle = MyTriangle()
-            1 -> mSquare = MySquare()
             2 -> mCube = MyCube()
-            3 -> mHexagonalPyramid = MyHexagonalPyramid()
-            4 -> {
-                mTexCube = MyTextCube(myContext)
-                mTexGround=MyTexGround(myContext)
-                mTexPillar= MyTexPillar(myContext)
-            }
-            5 -> mLitCube=MyLitCube(myContext)
         }
-
-        // initialize a trianlge
         mTrackball= MyTrackball()
     }
 
     override fun onDrawFrame(unused: GL10) {
-        // Redraw background color
         GLES30.glClear(GLES30.GL_COLOR_BUFFER_BIT or GLES30.GL_DEPTH_BUFFER_BIT)
-
-        // Set the camera position (View matrix)
-        //Matrix.setLookAtM(viewMatrix, 0, 0f, 2.5f,  3.5f, 0f, 0f, 0f, 0f, 1f, 0f)
-
-        // Calculate the projection and view transformation
-        //Matrix.multiplyMM(mvpMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
-
         when(drawMode) {
-            0 -> mTriangle.draw()
-            1 -> mSquare.draw()
             2 -> mCube.draw(mTrackball.rotationMatrix)
-            //3 -> mHexagonalPyramid.draw(mvpMatrix)
-            3 -> mHexagonalPyramid.draw(mTrackball.rotationMatrix)
-            4 -> {
-                mTexGround.draw(mTrackball.rotationMatrix)
-                mTexPillar.draw(mTrackball.rotationMatrix)
-                mTexCube.draw(mTrackball.rotationMatrix)
-
-            }
-            5->mLitCube.draw((mTrackball.rotationMatrix))
         }
-
     }
 
     override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
-        // 좌하단 (0,0), 캔버스 넓이를 width, height) (전체)
         GLES30.glViewport(0, 0, width, height)
-
-        // this projection matrix is applied to object coordinates in the onDrawFrame() method
-        /*if (width > height){
-            val ratio: Float = width.toFloat() / height.toFloat()
-            //Matrix.orthoM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 0f, 1000f)
-            Matrix.frustumM(projectionMatrix, 0, -ratio, ratio, -1f, 1f, 0.5f, 1000f)
-        }
-        else{
-            val ratio: Float = height.toFloat() / width.toFloat()
-            //Matrix.orthoM(projectionMatrix, 0, -1f, 1f,-ratio, ratio, 0f, 1000f)
-            Matrix.orthoM(projectionMatrix, 0, -1f, 1f,-ratio, ratio, 0f, 1000f)
-        }
-        
-        //val ratio: Float = width.toFloat()/height.toFloat()
-        //Matrix.perspectiveM(projectionMatrix, 0, 60f, ratio, .5f, 1000f)*/
         when(drawMode){
             2->mCube.resize(width,height)
-            3->mHexagonalPyramid.resize(width,height)
-            4->{
-                mTexCube.resize(width,height)
-                mTexGround.resize(width,height)
-                mTexPillar.resize(width,height)
-            }
-            5->mLitCube.resize(width,height)
         }
         mTrackball.resize(width,height)
     }
